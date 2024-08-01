263686a82b60610bbc4cf7a7b4ee2fad3ab15507
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-08-01 14:06:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Value equal to index value/value-equal-to-index-value.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+class Solution:
+    # Function to find values in array equal to their indices
+    def valueEqualToIndex(self, arr):
+        
+        return [i+1 for i in range(len(arr)) if arr[i]==i+1]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.valueEqualToIndex(arr)
+        if len(ans) == 0:
+            print("Not Found")
+        else:
+            for x in ans:
+                print(x, end=" ")
+            print()
+        tc -= 1
+
+# } Driver Code Ends
