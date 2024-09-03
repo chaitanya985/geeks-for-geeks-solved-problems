198f2b627e1a3a427852bb0b9987879150979467
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-03 15:19:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stickler Thief/stickler-thief.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:  
+    
+    #Function to find the maximum money the thief can get.
+    def FindMaxSum(self,a, n):
+        
+        # code here
+        incl, excl = a[0], 0
+        
+        for i in range(1, n):
+            
+            incl, excl= excl+a[i], max(incl, excl)
+            
+        return max(incl, excl)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+sys.setrecursionlimit(10**6)
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.FindMaxSum(a,n))
+# } Driver Code Ends
