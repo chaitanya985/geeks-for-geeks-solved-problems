58ac5ff18be718052e3515236ad6652f92dbb8b9
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-11 12:56:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing number/missing-number.py
@@ -0,0 +1,54 @@
+
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, n : int, arr : List[int]) -> int:
+        # code here
+        
+        total=0
+        
+        for i in arr:
+            
+            total+=i
+            
+        a=n*(n+1)//2
+        
+        missing_num = a - total
+        
+        return missing_num
+        
+
+
+
+#{ 
+ # Driver Code Starts
+
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(n-1)
+        
+        obj = Solution()
+        res = obj.missingNumber(n, arr)
+        
+        print(res)
+        
+
+# } Driver Code Ends
