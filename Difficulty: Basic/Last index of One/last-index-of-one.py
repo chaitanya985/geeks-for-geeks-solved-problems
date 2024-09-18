d9b7c560e985dfeee392031d47f4606098d32508
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-18 14:14:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last index of One/last-index-of-one.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def lastIndex(self, s: str) -> int:
+        # code here
+        
+        for i in range(len(s)-1, -1, -1):
+            
+            if s[i]==str(1):
+                
+                return i
+                
+        return -1
+        
+        
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while (T > 0):
+        s = input()
+        ob = Solution()
+        print(ob.lastIndex(s))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
